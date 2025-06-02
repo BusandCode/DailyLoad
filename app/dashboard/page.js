0d50717a72cb7453065b87aef34fc40d8ef4b565
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-06-02 16:17:56 +0100

    Dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -9,7 +9,7 @@ const page = () => {
       date:"1st of June",
       time:"9am",
       status:"pending",
-      createdAt:new Date ("20/04/20225")
+      createdAt:new Date ("20/04/2025")
     },
     {
       id:2,
@@ -17,8 +17,8 @@ const page = () => {
       description:"Never miss church services again",
       date:"2nd of June",
       time:"7am",
-      status:"In Progress",
-      createdAt:new Date ("21/04/20225")
+      status:"in progress",
+      createdAt:new Date ("21/04/2025")
     },
     {
       id:3,
@@ -26,13 +26,47 @@ const page = () => {
       description:"Sleep for 5 hours so as to wake up early",
       date:"3rd of June",
       time:"11pm",
-      status:"Completed",
-      createdAt:new Date ("22/04/20225")
+      status:"completed",
+      createdAt:new Date ("22/04/2025")
     },
 ]
   return (
-    <section className='h-full p-3 lg:w-3/5 lg:mx-auto'>
-      
+    <section className='bg-white w-full min-h-screen p-3 flex flex-col'>
+      <div className='lg:w-1/2 mx-auto'>
+      {/* Name of user */}
+      <div className='flex items-center gap-1'>
+        <div className='bg-[#11084a] p-2 text-[16px] text-white font-bold rounded-full w-[41px] h-[41px] flex justify-center items-center'>A</div>
+        <h1 className='text-[#11084a]'>Andrew Adetokunbo</h1>
+      </div>
+
+      <div className='flex flex-col justify-center gap-[20px] mt-5'>
+        {tasks.map(task=>(
+          <div key={task.id} >
+            <div className='flex flex-col gap-[10px] shadow-[#2d1b69] shadow-md rounded-md p-5'>
+              {/* Title */}
+              <div className='flex flex-col justify-center items-start'>
+              <label>Title</label>
+              <input type="text" className='h-[42px] text-[#2d1b69] border-[1px] border-[#2d1b69] w-full p-2 outline-0' placeholder={task.title}/>
+              </div>
+              {/* description */}
+              <div className='flex flex-col justify-center items-start'>
+                <label>Description</label>
+                <textarea name="" id="" className='border-[1px] border-[#2d1b69] text-[#2d1b69] w-full resize-none h-[100px] p-2 outline-0' placeholder={task.description}></textarea>
+              </div>
+              {/* Status */}
+              <div className='flex flex-col justify-center items-start'>
+                <label>Status</label>
+                <select name="status" className='h-[42px] text-[#2d1b69] border-[1px] border-[#2d1b69] w-full p-2 outline-0' defaultValue={task.status}>
+                  <option value="pending">Pending</option>
+                  <option value="in progress">In Progress</option>
+                  <option value="completed">Completed</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      </div>
     </section>
   )
 }
