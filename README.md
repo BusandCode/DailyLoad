5a70bc3ac8a2ba29b4fbb59d692f30aa4cc9b912
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-06-02 14:23:04 +0100

    Dashboard

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ADETOKUNBO ANDREW OLUWABUSAYOMI
 DailyLoad is a personalized task tracker built by BusandCode using Next.js, Firebase, and TailwindCSS to help you stay organized and productive every day. Designed with simplicity and functionality in mind, DailyLoad allows users to securely sign up and log in, then effortlessly create, read, update, and delete tasks. Smart email reminders ensure you never miss a deadline, keeping your daily workflow on track. Whether you're managing work, studies, or personal goals, DailyLoad provides a clean, responsive interface that makes task management seamless. Built for speed and reliability, DailyLoad is your go-to companion for achieving more with clarity and focus.
+
+Tech Stack
+Frontend: Next.js (Pages Router or App Router)
+Backend: Firebase (Auth, Firestore, Functions)
+Styling: TailwindCSS
+State: React Hooks
+
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,35 @@
 import React from 'react'
 
 const page = () => {
+  const tasks = [
+    {
+      id:1,
+      title:"Go to Library",
+      description:"Read SEN 302 for 2 hours and then SEN 304 for 3 hours",
+      date:"1st of June",
+      time:"9am",
+      status:"pending",
+      createdAt:new Date ("20/04/20225")
+    },
+    {
+      id:2,
+      title:"Go to Fellowship",
+      description:"Never miss church services again",
+      date:"2nd of June",
+      time:"7am",
+      status:"In Progress",
+      createdAt:new Date ("21/04/20225")
+    },
+    {
+      id:3,
+      title:"To go and sleep",
+      description:"Sleep for 5 hours so as to wake up early",
+      date:"3rd of June",
+      time:"11pm",
+      status:"Completed",
+      createdAt:new Date ("22/04/20225")
+    },
+]
   return (
     <section className='h-full p-3 lg:w-3/5 lg:mx-auto'>
       
